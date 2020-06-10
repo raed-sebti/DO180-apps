c68735391c65a3f3fa4cf35d60f24776bfde8c76
Student User <student@workstation.lab.example.com>
2020-06-10 15:06:39 +0000

    Ip pod added

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,5 @@
 <?php
-print "Hello, World Test  php version is " . PHP_VERSION . "\n";
+$host = gethostname();
+$ip = gethostbyname($host);
+print "Hello World! Pod Ip is $ip  php version is " . PHP_VERSION . "\n";
 ?>
